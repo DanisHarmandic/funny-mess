e8d98fe0563ad09b5158952a98e4ae0f4a542d0f
Danis Harmandic <122181138+DanisHarmandic@users.noreply.github.com>
2023-02-13 21:30:25 +0100

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Danis Harmandic on 4. 2. 2023..
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
