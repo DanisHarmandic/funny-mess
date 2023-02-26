104886bf0884128fc417eaf2a76dc666a5706112
Danis Harmandic <122181138+DanisHarmandic@users.noreply.github.com>
2023-02-26 12:27:17 +0100

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Danis Harmandic on 17. 2. 2023..
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
